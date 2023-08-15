98cb8fb31177e948de1d21b0de34c9014a736492
Edy010Reys <edson10vasconcelos@gmail.com>
2023-08-15 07:54:37 -0300

    Removendo pasta que contém erros

--- eletronics_store/bin/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:eletronics_store/eletronics_store.dart';
-
-void main() {
-  final store = ElectronicsStore();
-  store.loadProducts('data/products.json');
-  print('#### LISTA DE PRODUTOS\n');
-  store.listProducts();
-  print('Preço Total: ${store.totalPrice().toStringAsFixed(2)}');
-}
