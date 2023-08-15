92216269b610353338dfa564e97338dbe446ad26
Edy010Reys <edson10vasconcelos@gmail.com>
2023-08-15 07:20:32 -0300

    apenas teste

---
+++ pasta_teste/bin/pasta_teste.dart
@@ -0,0 +1,5 @@
+import 'package:pasta_teste/pasta_teste.dart' as pasta_teste;
+
+void main(List<String> arguments) {
+  print('Hello world: ${pasta_teste.calculate()}!');
+}
---
+++ pasta_teste/test/pasta_teste_test.dart
@@ -0,0 +1,8 @@
+import 'package:pasta_teste/pasta_teste.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
